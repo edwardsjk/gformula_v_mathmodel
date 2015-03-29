29fab655a34d0df4593c58067d454ae8de314ae3
Alex Keil <alex.keil@gmail.com>
2015-03-29 17:23:24 -0400

    Added dataset

--- data/makedata.R
+++ data/makedata.R
@@ -4,3 +4,9 @@ x = rbinom(100, 1, 1/(1+exp(-(-0.5 + z))))
 y = rbinom(100, 1, 1/(1+exp(-(-0.5 - 1 + z + 2*x))))
 
 write.csv(data.frame(z=z,x=x,y=y), "~/Documents/programming_examples/R/_mine/shiny/gformula_slider/data/testdata.csv", row.names=FALSE)
+
+set.seed(12123)
+z = rnorm(100, 0, 1)
+x = rnorm(100, 3*z, 1)
+y = rbinom(100, 1, 1/(1+exp(-(-0.5 - 1 + z + 2*x))))
+write.csv(data.frame(z=z,x=x,y=y), "~/Documents/programming_examples/R/_mine/shiny/gformula_slider/data/testdata2.csv", row.names=FALSE)
